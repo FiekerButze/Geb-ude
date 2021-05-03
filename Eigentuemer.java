f4612cb1bfc03b0f0a9998cc251eeeae6576a3d5
GZUZ 187 <JanzLeon05@ahfs-sus.de>
2021-05-03 08:54:57 +0200

    Dorfklasse

---
+++ Eigentuemer.java
@@ -0,0 +1,17 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Eigentümer.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Eigentuemer
+{
+        
+    
+    public Eigentuemer()
+    {
+        
+    }
+
+}
